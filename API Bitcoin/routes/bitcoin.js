af1291d85469c59795eeed83bf757052a9103758
Kelwinkxps13 <kelwinjhackson13@gmail.com>
2025-01-14 19:55:15 -0300

    modificadas poucas coisas

--- API Bitcoin/routes/bitcoin.js
+++ API Bitcoin/routes/bitcoin.js
@@ -3,38 +3,46 @@ var router = express.Router();
 var axios = require('axios'); 
 
 /* GET home page. */
-router.get('/usd', async function (req, res, next) {
+router.get('/:country', async function (req, res, next) {
 
     const bitcoin = await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
 
-    res.render('components/main', {
-        title: 'Crypto Data',
-        content: '../bitcoin',
-        b: bitcoin.data,
-        locate: 'USD'
-    });
+    if (req.params.country === "usd") {
+
+        res.render('components/main', {
+            title: 'Crypto Data',
+            content: '../bitcoin',
+            b: bitcoin.data,
+            locate: 'USD'
+        });
+        
+    }else if (req.params.country === "pound") {
+
+        res.render('components/main', {
+            title: 'Crypto Data',
+            content: '../bitcoin',
+            b: bitcoin.data,
+            locate: 'GBP'
+        });
+        
+    }else if (req.params.country === "euro") {
+        
+        res.render('components/main', {
+            title: 'Crypto Data',
+            content: '../bitcoin',
+            b: bitcoin.data,
+            locate: 'EUR'
+        });
+
+    }else{
+        res.json({err: 'Invalid Endpoint'})
+    }
+
+    
 });
-router.get('/pound', async function (req, res, next) {
 
-    const bitcoin = await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
-
-    res.render('components/main', {
-        title: 'Crypto Data',
-        content: '../bitcoin',
-        b: bitcoin.data,
-        locate: 'GBP'
-    });
-});
-router.get('/euro', async function (req, res, next) {
-
-    const bitcoin = await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json");
-
-    res.render('components/main', {
-        title: 'Crypto Data',
-        content: '../bitcoin',
-        b: bitcoin.data,
-        locate: 'EUR'
-    });
-});
+router.get('/*', function(req, res, next) {
+    res.json({err: 'Invalid Endpoint'})
+  });
 
 module.exports = router;
