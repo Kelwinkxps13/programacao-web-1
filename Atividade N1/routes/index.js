47b31aa28103b387c223eba56712b9e5b0895c83
Kelwin Jhackson <141432193+Kelwinkxps13@users.noreply.github.com>
2025-01-30 14:37:21 -0300

    adicionada a base do sistema de auth

--- Atividade N1/routes/index.js
+++ Atividade N1/routes/index.js
@@ -3,10 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('modulos/home', { 
-    title: 'Home', 
+  is_authenticated = true;
+  if (is_authenticated) {
+    res.render('modulos/home', { 
+      title: 'Home', 
+      content: '../modulos/home' 
+    });
+  }else{
+    res.redirect('/login1');
+  }
+  
+});
+router.get('/login1', function(req, res, next) {
+  res.render('login1', { 
+    title: 'Login', 
     content: '../modulos/home' 
   });
+});
+
+router.post('/validate1login', function(req, res, next) {
+  // validação
+  res.redirect('/login2'); 
+});
+router.get('/login2', function(req, res, next) {
+  res.render('login2', { 
+    title: 'Login', 
+    content: '../modulos/home' 
+  });
+});
+router.post('/validate2login', function(req, res, next) {
+  // validação
+  res.redirect('/'); 
 });
 
 module.exports = router;
